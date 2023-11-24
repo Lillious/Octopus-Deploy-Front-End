f4e059d17243f367b0ae2d373a60f943b93d5f6b
Logan Brown <logan.brown@tylertech.com>
2023-11-24 10:07:46 -0500

    Added swagger documentation at /docs

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,27 @@ import express from 'express';
 import path from 'path';
 import cookieParser from "cookie-parser";
 import cookieSession from "cookie-session";
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
 const app = express();
 const port = process.env.PORT || 8080;
 const host = process.env.HOST || "0.0.0.0";
+
+// Swagger Documentation
+const options: swaggerJsdoc.Options = {
+  definition: {
+      openapi: "3.0.0",
+      info: {
+          title: "Octopus Deploy Authentication REST API",
+          version: "1.0.0",
+          description: "An API for authenticating with Octopus Deploy",
+      },
+  },
+  apis: ["./src/routes/*.ts"],
+};
+
+const swaggerSpec = swaggerJsdoc(options);
+const docsPath = "/docs";
 
 // Server configuration
 app.use(express.json());
@@ -25,6 +43,13 @@ app.use(
   })
 );
 
+// Swagger Documentation
+app.use(docsPath, swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.get("/swagger.json", (req: express.Request, res: express.Response) => {
+    res.setHeader("Content-Type", "application/json");
+    res.send(swaggerSpec);
+});
+
 // Authentication Middleware API
 import { router as auth } from './routes/auth';
 app.use(auth);
@@ -37,12 +62,13 @@ app.use(api);
 app.use("/",express.static(path.join(__dirname, "/www/public/")));
 app.use("/dashboard",express.static(path.join(__dirname, "/www/dashboard/")));
 
-app.get('*', (req, res) => {
-    res.status(404).redirect("/");
-});
-
 app.listen(port as number, host as string, () => {
     console.log(`Server started at http://localhost:${port}`);
+    console.log(`Swagger docs available at http://localhost:${port}${docsPath}`);
+});
+
+app.use("*", (req, res) => {
+    res.status(404).redirect("/");
 });
 
 // Create the databases and tables
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -26,6 +26,21 @@ if (serverHash !== clientHash) {
 }
 
 router.post(`/generate-api-key`, (req, res) => {
+    /**
+     * @openapi
+     * /generate-api-key:
+     *  post:
+     *      tags:
+     *      - Default
+     *      description: Generate an API Key
+     *      responses:
+     *          200:
+     *              description: API Key generated successfully
+     *          403:
+     *              description: Invalid session
+     *          400:
+     *              description: Invalid credentials
+     */
     if (!req?.headers?.session) return res.status(403).send({
         error: "INVALID_SESSION"
     });
@@ -57,35 +72,137 @@ router.post(`/generate-api-key`, (req, res) => {
 
 // Accounts API
 router.get(`${apiPath}/accounts`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/accounts':
+     *  get:
+     *      description: Get a list of accounts
+     *      tags:
+     *      - Accounts
+     *      responses:
+     *          200:
+     *              description: List of accounts
+     *          400:
+     *              description: Invalid credentials
+     *          403:
+     *              description: Invalid session
+     *          500:
+     *              description: Internal server error
+     */
     const result = await Octopus.Account.List();
     res.send(result);
 });
 
 // Spaces API
 router.get(`${apiPath}/spaces`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/spaces':
+     *  get:
+     *      description: Get a list of spaces
+     *      tags:
+     *      - Spaces
+     *      responses:
+     *          200:
+     *              description: List of spaces
+     *          400:
+     *              description: Invalid credentials
+     *          403:
+     *              description: Invalid session
+     *          500:
+     *              description: Internal server error
+     */
     const result = await Octopus.Space.List();
     res.send(result);
 });
 
 // Deployment API
 router.get(`${apiPath}/deployments`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/deployments':
+     *  get:
+     *      description: Get a list of deployments
+     *      tags:
+     *      - Deployments
+     *      responses:
+     *          200:
+     *              description: List of deployments
+     *          400:
+     *              description: Invalid credentials
+     *          403:
+     *              description: Invalid session
+     *          500:
+     *              description: Internal server error
+     */
     const result = await Octopus.Deployment.List();
     res.send(result);
 });
 
 // Deployment Process API
 router.get(`${apiPath}/deployment-processes`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/deployment-processes':
+     *  get:
+     *      description: Get a list of deployment processes
+     *      tags:
+     *      - Deployment Target
+     *      responses:
+     *          200:
+     *              description: List of deployment processes
+     *          400:
+     *              description: Invalid credentials
+     *          403:
+     *              description: Invalid session
+     *          500:
+     *              description: Internal server error
+     */
     const result = await Octopus.DeploymentProcess.List();
     res.send(result);
 });
 
 // Deployment Target API
 router.get(`${apiPath}/deployment-targets`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/deployment-targets':
+     *  get:
+     *      description: Get a list of deployment targets
+     *      tags:
+     *      - Deployment Target
+     *      responses:
+     *          200:
+     *              description: List of deployment targets
+     *          400:
+     *              description: Invalid credentials
+     *          403:
+     *              description: Invalid session
+     *          500:
+     *              description: Internal server error
+     */
     if (!req?.query?.id) {
         const result = await Octopus.DeploymentTarget.List();
         return res.send(result);
     }
 
+    /**
+     * @openapi
+     * '/api/v1/deployment-targets{id}':
+     *  get:
+     *      description: Get a list of deployment targets by ID
+     *      tags:
+     *      - Deployment Target
+     *      responses:
+     *          200:
+     *              description: List of deployment targets
+     *          400:
+     *              description: Invalid credentials
+     *          403:
+     *              description: Invalid session
+     *          500:
+     *              description: Internal server error
+     */
     try {
         const result = await Octopus.DeploymentTarget.Find(req.query.id as string);
         return res.status(200).send(result);
@@ -98,6 +215,24 @@ router.get(`${apiPath}/deployment-targets`, async (req, res) => {
 
 // Upgrade Deployment Target
 router.post(`${apiPath}/deployment-target-upgrade`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/deployment-target-upgrade{id}{space}':
+     *  post:
+     *      description: Upgrade a deployment target
+     *      tags:
+     *      - Deployment Target
+     *      responses:
+     *          200:
+     *              description: Deployment target upgraded successfully
+     *          400:
+     *              description: Invalid credentials
+     *          403:
+     *              description: Invalid session
+     *          500:
+     *              description: Internal server error
+     */
+
     if (!req?.body?.id) return res.status(400).send({
         error: "ID is required"
     });
@@ -117,6 +252,23 @@ router.post(`${apiPath}/deployment-target-upgrade`, async (req, res) => {
 });
 
 router.get(`${apiPath}/deployment-history`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/deployment-history{id}{space}':
+     *  get:
+     *      description: Get a list of deployment history
+     *      tags:
+     *      - Deployment Target
+     *      responses:
+     *          200:
+     *              description: List of deployment history
+     *          400:
+     *              description: Invalid credentials
+     *          403:
+     *              description: Invalid session
+     *          500:
+     *              description: Internal server error
+     */
     if (!req?.query?.id) return res.status(400).send({
         error: "ID is required"
     });
@@ -131,6 +283,23 @@ router.get(`${apiPath}/deployment-history`, async (req, res) => {
 
 // Tasks API
 router.get(`${apiPath}/deployment-tasks`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/deployment-tasks{id}':
+     *  get:
+     *      description: Get a list of deployment tasks
+     *      tags:
+     *      - Tasks
+     *      responses:
+     *          200:
+     *              description: List of deployment tasks
+     *          400:
+     *              description: Invalid credentials
+     *          403:
+     *              description: Invalid session
+     *          500:
+     *              description: Internal server error
+     */
     if (!req?.query?.id) return res.status(400).send({
         error: "ID is required"
     });
@@ -141,6 +310,24 @@ router.get(`${apiPath}/deployment-tasks`, async (req, res) => {
 
 // Create Task
 router.post(`${apiPath}/deployment-task`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/deployment-task{task}':
+     *  post:
+     *      description: Create a deployment task
+     *      tags:
+     *      - Tasks
+     *      responses:
+     *          200:
+     *              description: Deployment task created successfully
+     *          400:
+     *              description: Invalid credentials
+     *          403:
+     *              description: Invalid session
+     *          500:
+     *              description: Internal server error
+     */
+
     if (!req?.body?.task) return res.status(400).send({
         error: "Task is required"
     });
@@ -151,6 +338,23 @@ router.post(`${apiPath}/deployment-task`, async (req, res) => {
 
 // Re-run Task
 router.post(`${apiPath}/deployment-task-rerun`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/deployment-task-rerunP{id}':
+     *  post:
+     *      description: Re-run a deployment task
+     *      tags:
+     *      - Tasks
+     *      responses:
+     *          200:
+     *              description: Deployment task re-run successfully
+     *          400:
+     *              description: Invalid credentials
+     *          403:
+     *              description: Invalid session
+     *          500:
+     *              description: Internal server error
+     */
     if (!req?.body?.id) return res.status(400).send({
         error: "ID is required"
     });
@@ -161,24 +365,86 @@ router.post(`${apiPath}/deployment-task-rerun`, async (req, res) => {
 
 // Environment API
 router.get(`${apiPath}/environments`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/environments':
+     *  get:
+     *      description: Get a list of environments
+     *      tags:
+     *      - Environments
+     *      responses:
+     *          200:
+     *              description: List of environments
+     *          400:
+     *              description: Invalid credentials
+     *          403:
+     *              description: Invalid session
+     *          500:
+     *              description: Internal server error
+     */
     const result = await Octopus.Environment.List();
     res.send(result);
 });
 
 // Event API
 router.get(`${apiPath}/events`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/events':
+     *  get:
+     *      description: Get a list of events
+     *      tags:
+     *      - Events
+     *      responses:
+     *          200:
+     *              description: List of events
+     *          400:
+     *              description: Invalid credentials
+     *          500:
+     *              description: Internal server error
+     */
     const result = await Octopus.Event.List();
     res.send(result);
 });
 
 // Feed API
 router.get(`${apiPath}/feeds`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/feeds':
+     *  get:
+     *      description: Get a list of feeds
+     *      tags:
+     *      - Feeds
+     *      responses:
+     *          200:
+     *              description: List of feeds
+     *          400:
+     *              description: Invalid credentials
+     *          500:
+     *              description: Internal server error
+     */
     const result = await Octopus.Feed.List();
     res.send(result);
 });
 
 // Connection API
 router.get(`${apiPath}/check-connection`, async (req, res) => {
+    /**
+     * @openapi
+     * '/api/v1/check-connection{id}':
+     *  get:
+     *      description: Check a connection
+     *      tags:
+     *      - Deployment Target
+     *      responses:
+     *          200:
+     *              description: Connection checked successfully
+     *          400:
+     *              description: Invalid ID
+     *          500:
+     *              description: Internal server error
+     */
     try {
         if (!req?.query?.id) return res.status(400).send({
             error: "ID is required"
